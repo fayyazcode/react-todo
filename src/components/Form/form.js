b84fbdbc452ba359895b8bea89ac32fc26a115e6
Fayyaz Alam <fayazalam.98@gmail.com>
2021-09-17 08:04:19 -0700

    form input and add button

---
+++ src/components/Form/form.js
@@ -0,0 +1,26 @@
+import React from "react"; 
+
+const Form =({inputText,setInputText,todos,setTodos})=>{
+const inputTextHandler=(e)=>{
+
+setInputText(e.target.value)
+};
+const submitTodoHandler = (e) => {
+  e.preventDefault(); 
+setTodos([
+...todos,{text: inputText ,completed :false, id: Math.random()*100}
+]);
+setInputText("");
+};
+
+    return(
+        <form>
+        <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
+        <button onClick={submitTodoHandler} className="todo-button" type="submit">
+          <i className="fas fa-plus-square"></i>
+        </button>
+   
+      </form>
+    );
+};
+export default Form;
