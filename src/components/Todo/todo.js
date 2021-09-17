eb16ddef46c721f1490a21483dfb44789a983833
Fayyaz Alam <fayazalam.98@gmail.com>
2021-09-17 08:05:39 -0700

    todo list item and delete functionality

---
+++ src/components/Todo/todo.js
@@ -0,0 +1,19 @@
+import React from "react";
+const Todo=({todo,text,todos,setTodos})=>{
+    console.log(todo)
+    const deleteHandler=()=>{
+        setTodos(todos.filter(el => el.id !== todo.id))
+    }
+    return(
+<div className="todo">
+    <li className="todo-item" >{text}</li>
+    <button className="complete-btn">
+        <i className="fas fa-edit"></i>
+    </button>
+    <button className="trash-btn" onClick={deleteHandler}>
+        <i className="fas fa-trash"></i>
+    </button>
+</div>
+    );
+};
+export default Todo;
